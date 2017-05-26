7b14ff6860bc55cd6a43352761715a32e4254f32
Galen Church <galenechurch@gmail.com>
2017-05-26 14:40:17 -0400

    commenting

--- shutdown/shutdown.py
+++ shutdown/shutdown.py
@@ -1,9 +1,29 @@
-#port of status.cpp
+"""
+    shutdown
+
+    ~~~~~~~~~
+
+    A module for shutting down the HWR via executrion of the bash script.
+
+    The HW interface is a off-mom swith pulling the SHUTDOWN_SW pin low for >=3 sec.
+
+    :author: Galen Church
+    :copyright: 2017
+
+"""
+
+# ------------------------------------------------
+#   Imports
+# ------------------------------------------------
+
 
 import time
 import subprocess
 import RPi.GPIO as GPIO
 
+# ------------------------------------------------
+# Classes
+# ------------------------------------------------
 
 class Shutdown:
     
@@ -13,21 +33,36 @@ class Shutdown:
         self.SW = SHUTDOWN_SW
         self.down = 0
         self.up = 0
+
+        #set GPIO to reference IO Pins based on BCM convention
         GPIO.setmode(GPIO.BCM)
+        
+        #set SHUTDOWN_SW to input with internal pull-up
         GPIO.setup(SHUTDOWN_SW, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+        #configure inturrupts to trigger of both rising and falling edges
         GPIO.add_event_detect(SHUTDOWN_SW, GPIO.BOTH, callback=self.shutSWISR)
 
 
     def shutSWISR(self, chan):
+        """Callback function acting as ISR.  Sets inturrupt flag.
+        """
         self.shutdown_pressed = 1
 
     def todo(self):
-        #args = ["sudo", "shutdown"to, "now"]
+        """Function executed when shutdown conditions are met.  
+        Currently executes tool_operator_down.sh as root in current dir
+        """
         args = ["sudo", "sh", "tool_operator_down.sh"]
         print subprocess.call(args)
 
 
     def logic(self):
+        """The logic of the shutdown loop without the loop.
+        state = 0: First inturrupt tripped set self.down with start of 3sec count.
+        state = 1: waiting for 3sec to pass to execure self.todo().  
+            if inturrupt triggered and self.SW is High, i.e. button was released, set state = 0
+        """
         if self.shutdown_pressed and (self.state ==  0):
             if not GPIO.input(self.SW):
                 self.down = time.clock()
@@ -54,6 +89,8 @@ class Shutdown:
         return 1
 
     def loop(self):
+        """Lazy loop for testing.  
+        """
         cont = 1
         while cont:
-            cont = self.logic()
+            cont = self.logic()   
--- shutdown/test.py
+++ shutdown/test.py
@@ -1,7 +1,12 @@
 #test.py
 from shutdown import Shutdown
 
-S = Shutdown(20)
+#Pin 20 as referenced by BCM Documentation.
+# (Corresponds to physical pin 38)
+
+swtich_BCM_pin = 20
+
+S = Shutdown(swtich_BCM_pin)
 
 to_continute = 1
 while to_continute:
